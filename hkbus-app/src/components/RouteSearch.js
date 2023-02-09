32ab2f393279ad861a59d7468eb18c548e92a6bf
ynt423 <oscar42302@gmail.com>
2023-02-09 21:17:06 +0800

    u

--- hkbus-app/src/components/RouteSearch.js
+++ hkbus-app/src/components/RouteSearch.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const RouteSearch = () => {
-  return <div>RouteSearch</div>;
+  return (
+    <div>
+      RouteSearch
+      <div className="RouteSection">
+        <div className="index-title">路線搜尋</div>
+        <div className="index-content">
+          <div className="routesearch"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RouteSearch;
