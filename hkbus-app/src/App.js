0ff7e8ff07e645a36125f100edec01841419e60b
Dominic Chow <dominicchow83@yahoo.com.hk>
2023-02-11 21:27:40 +0800

    123

--- hkbus-app/src/App.js
+++ hkbus-app/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import index from './index'
-import map from './map'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import React from "react";
+import index from "./index";
+import map from "./map";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Link to="/">Home</Link><br />
-      <Link to="/map">Map</Link>
-    <Routes>
-      <Route path="/" element={<index />} />
-      <Route path="/map" element={<map />} />
-      <Route path="*" element={<h1>404</h1>} />
-    </Routes>    
-    </BrowserRouter>
-  )
+    <React.Fragment>
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/map">Map</Link>
+        <Routes>
+          <Route path="/" element={<index />} />
+          <Route path="/map" element={<map />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
