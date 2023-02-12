1d6db74654d55ba19c227418f06a17e066b143ee
Dominic Chow <dominicchow83@yahoo.com.hk>
2023-02-12 14:36:18 +0800

    123

--- hkbus-app/src/index.js
+++ hkbus-app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import map from "./map";
 import App from "./App";
-import RouteSearch from "./components/RouteSearch";
+import RouteSearch from "./components/routeSearch";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
