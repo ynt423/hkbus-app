5976be7f1f2bf948e9f8edb9133c0b5767890434
less2771 <74804307+less2771@users.noreply.github.com>
2023-02-12 21:36:53 +0800

    change navbar

--- hkbus-app/src/App.js
+++ hkbus-app/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import index from "./index";
 import map from "./map";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
-        <Link to="/">Home</Link>
-        <br />
-        <Link to="/map">Map</Link>
+
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">hkbus-app</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/map">Map</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
         <Routes>
           <Route path="/" element={<index />} />
           <Route path="/map" element={<map />} />
